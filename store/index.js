72e2bb8acb4fdff42a8b72da146409be76c62ff5
lianjy357 <lianjy357@qq.com>
2019-11-14 18:24:20 +0800

    增加KR新增页面

--- store/index.js
+++ store/index.js
@@ -9,16 +9,22 @@ const store = new Vuex.Store({
 	state: {
 		hasLogin: false, // 是否处于登录状态
 		userInfo: {}, // 登录用户信息
-		
 		OKRInfo: [], // OKR信息
+		member: [ // 成员
+			{username: 'lianjy', nickname: '大白'},
+			{username: 'wangy', nickname: '小雨'}
+		],
+
+		
 		OKRType: [
 			{type: 0, name: '成员', bgc: 'bg-red', bgc_pro1: 'bg-yellow', bgc_pro2: 'bg-green'},
 			{type: 1, name: '家庭', bgc: 'bg-blue', bgc_pro1: 'bg-olive', bgc_pro2: 'bg-green'}
 		],
-		member: [ // 成员
-			{username: 'lianjy', nickname: '大白'},
-			{username: 'wangy', nickname: '小雨'}
+		KRType: [
+			{type: 'NUM', name: '数字型', context: '发表公众号5篇文章'},
+			{type: 'PER', name: '百分比型', context: '学习视频《小王子》，完成63%进度'},
 		]
+		
 	},
 	mutations: {
 		// 登录
